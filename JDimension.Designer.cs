4999fb1dd4f12e68d33a335369bb0b8528f444eb
jnest <josh.nester@live.com>
2014-01-05 11:06:33 +1100

    Cleanup. Removed unused files.

--- JDimension.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Default code generation is disabled for model 'C:\Users\Josh\Documents\Visual Studio 2012\Projects\MechApp\J Dimension\JDimension.edmx'. 
-// To enable default code generation, change the value of the 'Code Generation Strategy' designer
-// property to an alternate value. This property is available in the Properties Window when the model is
-// open in the designer.
--- MechBuilder.aspx
+++
@@ -1,6 +0,0 @@
-﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="MechBuilder.aspx.cs" Inherits="J_Dimension.MechBuilder"  MasterPageFile="~/Site1.Master"%>
-
-
-<asp:Content ContentPlaceHolderID="Content" runat="server">
-    
-</asp:Content>
